897ab697d6cd373553fe14e4dc854c821ed1cef6
lipaiyevhen <lipayjeka@gmail.com>
2025-10-29 16:38:53 +0100

    Update managerCanLogin.spec.js

--- tests/manager/bankManagerLogin/managerCanLogin.spec.js
+++ tests/manager/bankManagerLogin/managerCanLogin.spec.js
@@ -1,13 +1,17 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import BankHomePage from '../../../src/pages/BankHomePage';
+import BankManagerMainPage from '../../../src/pages/manager/BankManagerMainPage';
 
 test('Assert manager can Login', async ({ page }) => {
-  /* 
-  Test:
-  1. Open Wizard bank home page 
-    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login
-  2. Click [Bank Manager Login]
-  3. Assert button [Add Customer] is visible
-  4. Assert button [Open Account] is visible
-  5. Assert button [Customers] is visible
-  */
+  const bankHomePage = new BankHomePage(page);
+  const managerMainPage = new BankManagerMainPage(page);
+
+  await bankHomePage.open();
+  await bankHomePage.clickManagerLoginButton();
+
+  await expect(page).toHaveURL(/.*manager/);
+  await expect(managerMainPage.addCustomerButton).toBeVisible();
+  await expect(managerMainPage.openAccountButton).toBeVisible();
+  await expect(managerMainPage.customersButton).toBeVisible();
 });
