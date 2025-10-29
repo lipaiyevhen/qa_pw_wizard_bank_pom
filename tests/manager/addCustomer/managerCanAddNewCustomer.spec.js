ee231918f953e546b85336fc4fb46973149ea43d
lipaiyevhen <lipayjeka@gmail.com>
2025-10-29 16:38:01 +0100

    Refactor test to use faker for customer data

--- tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
+++ tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
@@ -1,29 +1,29 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
+import BankHomePage from '../../../src/pages/BankHomePage';
+import AddCustomerPage from '../../../src/pages/manager/AddCustomerPage';
+import CustomersListPage from '../../../src/pages/manager/CustomersListPage';
 
 test('Assert manager can add new customer', async ({ page }) => {
-  /* 
-  Test:
-  1. Open add customer page by link
-    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/addCust
-  2. Fill the First Name.  
-  3. Fill the Last Name.
-  4. Fill the Postal Code.
-  5. Click [Add Customer].
-  6. Reload the page (This is a simplified step to close the popup)
-  7. Click [Customers] button.
-  8. Assert the customer First Name is present in the table in the last row. 
-  9. Assert the customer Last Name is present in the table in the last row. 
-  10. Assert the customer Postal Code is present in the table in the last row. 
-  11. Assert there is no account number for the new customer in the last row. 
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const postCode = faker.location.zipCode('#####');
 
-  Tips:
-  1. Use faker for test data generation, example usage:
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.LastName();
-    const postCode = faker.location.zipCode(); 
+  const bankHomePage = new BankHomePage(page);
+  const addCustomerPage = new AddCustomerPage(page);
+  const customersPage = new CustomersListPage(page);
 
-  2. Do not rely on the customer row id for the steps 8-11. 
-    Use the ".last()" locator to get the last row.
-  */
+  await bankHomePage.open();
+  await bankHomePage.clickManagerLoginButton();
+
+  await addCustomerPage.open();
+  // attach dialog handler BEFORE action in test
+  page.once('dialog', dialog => dialog.accept());
+  await addCustomerPage.addCustomer(firstName, lastName, postCode);
+
+  // ensure list is updated
+  await page.reload();
+  await customersPage.open();
+  await customersPage.searchCustomer(firstName);
+  await customersPage.verifyCustomerIsPresent(`${firstName} ${lastName}`);
 });
